b226f59fdeeae119d409c48fcd88b3805bd70db3
jawad-elhajjami <jawadelhajjamiofficiel@gmail.com>
2024-06-01 17:04:23 +0100

    Role based access integration

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $adminRole = Role::where('name', 'admin')->first();
+
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'role_id' => $adminRole->id,
+            'non_deletable' => true,
+        ]);
+
+        $user->save();
+    }
+}
---
+++ resources/views/components/authentication-card-logo.blade.php
@@ -0,0 +1,3 @@
+<a href="/">
+    <img src="{{ asset('images/logo.png') }}" width="100px">
+</a>
